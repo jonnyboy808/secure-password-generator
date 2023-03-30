9b3158580629e343843d51df73ac3370c9eb596a
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-03-29 19:13:47 -0700

    removed for function and replaced with if function for pass length

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  
+  passwordText.value = password;
+  return;
+
+}
+
 // List of character options
-var lowerCaseAlph = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCaseAplh = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "~", ",", ".", ";", "=", "<", ">", "/", "?", "+"];
+var lowerCaseAlph = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseAplh = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialCharacter = "!@#$%^&*()_-~,.;=<>+?";
 
 var lowerCaseAlph = [];
 var upperCaseAplh = [];
 var numbers = [];
 var specialCharacter = [];
 
+// create password function
+function generatePassword () {
+    var outputPassword = "";
 
-// Write password to the #password input
-function writePassword() {
-  var password = prompt('Enter password length between 8 and 126');
-  var passwordLengthConfirmation = confirm(
-    'You selected ' + password + ', is this correct?'
-  );
-  
+    var passwordLength = prompt ("Select Length between 8 to 128 characters");
+    if (passwordLength !=null) {
+        if (passwordLength >=8 && passwordLength <= 128) {
 
+            alert("Please choose one upper, lower, number, and special");
 
-
-
-
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-for (var i = 0; i < numbers.length; i++) {
-    numbers.push(numbers[i]);
+            var
+        }
+    }
 }
 
-for (var i = 0; i < specialCharacter.length; i++) {
-    specialCharacter.push(specialCharacter[i]);
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
